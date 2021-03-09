b293d3efb72bedbb70b13d2afbc8f54885c595e2
Brayan Garcia <bgarcia.95@outlook.com>
2021-03-08 21:29:30 -0600

    Add TS config and libraries. Project until section 5 class 64

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+class Department {
+  // private id: string;
+  // private name: string;
+  private employees: string[] = [];
+
+  constructor(private id: string, public name: string) {
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmploye(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department("d1", "Accounting");
+
+accounting.addEmploye('Max');
+accounting.addEmploye('Manu');
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+// accountingCopy.describe();
